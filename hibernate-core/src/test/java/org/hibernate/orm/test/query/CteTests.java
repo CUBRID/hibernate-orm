7b7aa20e1271c67447d1d231cf41815de0c22029
Srltas <kdm97912@gmail.com>
2026-09-23 11:01:48 +0900

    HHH-20885 Measure the CUBRID CTE placement in an A/B CI matrix

--- hibernate-core/src/test/java/org/hibernate/orm/test/query/CteTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/query/CteTests.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.function.Consumer;
 
-import org.hibernate.community.dialect.CUBRIDDialect;
 import org.hibernate.community.dialect.InformixDialect;
 import org.hibernate.dialect.SybaseASEDialect;
 import org.hibernate.community.dialect.TiDBDialect;
@@ -196,7 +195,6 @@ public class CteTests {
 	@Test
 	@SkipForDialect(dialectClass = SybaseASEDialect.class, reason = "The emulation of CTEs in subqueries results in correlation in nesting level 2, which is not possible with Sybase ASE")
 	@SkipForDialect(dialectClass = TiDBDialect.class, reason = "The TiDB version on CI seems to be buggy")
-	@SkipForDialect(dialectClass = CUBRIDDialect.class, reason = "CUBRIDDialect declares CTE placement as SUBQUERY, so the with clause is hoisted to the top of the statement where CUBRID cannot resolve the outer query alias it still references")
 	public void testSubquery(SessionFactoryScope scope) {
 		scope.inTransaction(
 				session -> {
